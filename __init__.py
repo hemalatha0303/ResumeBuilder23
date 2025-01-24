72b8ac0d875d278e022db7c14a21c41b4fb3f791
hemalatha0303 <hemalathareddy.7254@gmail.com>
2025-01-24 07:48:22 +0530

    Your commit message here

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Root@123@localhost/resume_app'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Root@40123@localhost/resume_app'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Root@123'
 db = SQLAlchemy(app)
